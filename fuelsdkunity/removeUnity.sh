a337af388ffc88e421b12be0c5b592b2c54e0158
Alan Price <alan@fuelpowered.com>
2015-09-22 12:03:19 -0700

    making fuelsdk outputs be single filenames without the target type (remove -release)

--- fuelsdkunity/removeUnity.sh
+++ fuelsdkunity/removeUnity.sh
@@ -1,3 +1,3 @@
 echo "Libraries to delete ..."
-unzip -vl build/outputs/aar/fuelsdkunity-debug.aar  "libs/*"
-zip --delete build/outputs/aar/fuelsdkunity-debug.aar "libs/*"
+unzip -vl build/outputs/aar/fuelsdkunity.aar  "libs/*"
+zip --delete build/outputs/aar/fuelsdkunity.aar "libs/*"
