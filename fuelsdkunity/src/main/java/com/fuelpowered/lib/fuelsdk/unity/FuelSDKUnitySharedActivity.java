3d37c7ad9621e407ebdf9c2efd76c2c2841715cc
Hernan Zambrano <hernan@fuelpowered.com>
2016-12-12 16:40:56 -0800

    now we sending a new message wen the vg sync fail

--- fuelsdkunity/src/main/java/com/fuelpowered/lib/fuelsdk/unity/FuelSDKUnitySharedActivity.java
+++ fuelsdkunity/src/main/java/com/fuelpowered/lib/fuelsdk/unity/FuelSDKUnitySharedActivity.java
@@ -44,6 +44,7 @@ public class FuelSDKUnitySharedActivity {
 		mIntentFilter.addAction(fuelbroadcasttype.FSDK_BROADCAST_VG_LIST.toString());
 		mIntentFilter.addAction(fuelbroadcasttype.FSDK_BROADCAST_VG_ROLLBACK.toString());
 		mIntentFilter.addAction(fuelbroadcasttype.FSDK_BROADCAST_VG_CONSUME_SUCCESS.toString());
+		mIntentFilter.addAction(fuelbroadcasttype.FSDK_BROADCAST_VG_SYNC_FAIL.toString());
 		mIntentFilter.addAction(fuelbroadcasttype.FSDK_BROADCAST_NOTIFICATION_ENABLED.toString());
 		mIntentFilter.addAction(fuelbroadcasttype.FSDK_BROADCAST_NOTIFICATION_DISABLED.toString());
 
